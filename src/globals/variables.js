060510f5d5d826238653736a8f4a5ce5776156fb
kgiles <info@kgiles.com>
2020-04-18 14:06:44 -0700

    Added globals folder, utilities folder, and variables.js
    KG Commit - Added API key to variables.js

---
+++ src/globals/variables.js
@@ -0,0 +1,15 @@
+// Global variables
+
+// Movie DB API info
+
+// URL: https://www.themoviedb.org/settings/api
+// API key: b6950b3810471cf1d9e1e23f46669181
+
+// API Variables
+export const API_KEY = 'b6950b3810471cf1d9e1e23f46669181';
+export const API_DATA_MODE = '&mode=json';
+
+// General App Settings
+export const APP_FOLDER_DATE = '/movie-app';
+
+
