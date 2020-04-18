62bf5b1a6f3e28831c1647d66292e0f45d267154
kgiles <info@kgiles.com>
2020-04-18 16:04:56 -0700

    Connected to movie db API on individual movie page
    KG commit - Connected to movie database API on /individual-movie

--- src/globals/variables.js
+++ src/globals/variables.js
@@ -9,6 +9,7 @@
 // API Variables
 export const API_KEY = 'b6950b3810471cf1d9e1e23f46669181';
 export const API_DATA_MODE = '&mode=json';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 
 // General App Settings
 export const APP_FOLDER_DATE = '/movie-app';
