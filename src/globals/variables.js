01acf3b487b83fecba4815bc4eb03b3c0c93ba6e
Faris <fasic@my.bcit.ca>
2020-04-19 18:49:12 -0700

    home page

--- src/globals/variables.js
+++ src/globals/variables.js
@@ -10,8 +10,10 @@
 export const API_KEY = 'b6950b3810471cf1d9e1e23f46669181';
 export const API_DATA_MODE = '&mode=json';
 export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const POPULAR_URL = 'https://api.themoviedb.org/3/movie/popular/';
+
 
 // General App Settings
 export const APP_FOLDER_NAME = '/movie-app';
-export const MAX_NUMBER_OF_MOVIES = 12;
+
 
