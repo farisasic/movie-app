e9b938912702491526c90c50e086bbe4b0fcb467
kgiles <info@kgiles.com>
2020-04-22 00:22:40 -0700

    Progress on Favourites page
    Created token and session ids from API. General progress update on Favourites page.

--- src/globals/variables.js
+++ src/globals/variables.js
@@ -11,9 +11,14 @@ export const API_KEY = 'b6950b3810471cf1d9e1e23f46669181';
 export const API_DATA_MODE = '&mode=json';
 export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 export const POPULAR_URL = 'https://api.themoviedb.org/3/movie/popular';
+export const FAV_URL = 'https://api.themoviedb.org/3/account/';
 
 
 // General App Settings
 export const APP_FOLDER_NAME = '/movie-app';
+
+// LocalStorage Defaults
+export const STORAGE_CURRENT_MOVIES = 'movie-app-current-movies';
+export const STORAGE_FAV_MOVIES = 'movie-app-favourites';
 
 
